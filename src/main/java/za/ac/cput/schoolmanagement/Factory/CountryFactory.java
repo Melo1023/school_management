2c01a86c1c2852b599df140d78aa23920187c045
Lihle_N <218193882@mycput.ac.za>
2022-06-10 23:46:34 +0200

    Implemented Country factory class

---
+++ src/main/java/za/ac/cput/schoolmanagement/Factory/CountryFactory.java
@@ -0,0 +1,23 @@
+package za.ac.cput.schoolmanagement.Factory;
+/*
+ @Author: Lihle Njobe
+ Student number : 218193882
+ This is Country factory class
+ Due date: 18 June 2022
+ */
+
+import za.ac.cput.schoolmanagement.Domain.Country;
+import za.ac.cput.schoolmanagement.Helper.Helper;
+
+public class CountryFactory {
+
+    public static Country createCountry(String name) {
+     String id = Helper.generateId();
+     Country country = new Country.Builder()
+             .setId(id)
+             .setName(name)
+             .build();
+     return country;
+    }
+}
+
