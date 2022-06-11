547682795b57c522b127e42c3c85bbdff19b399c
ianlouw <ianlouw1997@gmail.com>
2022-06-11 23:40:22 +0200

    Added test class for employeeFactory and EmployeeAddressFactory. Added Irepository and employeeRepository and EmployeeAddressRepository

--- src/main/java/za/ac/cput/schoolmanagement/factory/EmployeeAddressFactory.java
+++ src/main/java/za/ac/cput/schoolmanagement/factory/EmployeeAddressFactory.java
@@ -1,14 +1,25 @@
 package za.ac.cput.schoolmanagement.factory;
 
+/*
+    Student: Ian Louw
+    Student Number: 216250773
+    Class for EmployeeAddressFactory.
+    Date: 11 June 2022
+ */
+
+import za.ac.cput.schoolmanagement.domain.Address;
 import za.ac.cput.schoolmanagement.domain.EmployeeAddress;
 import za.ac.cput.schoolmanagement.helper.Helper;
 
 public class EmployeeAddressFactory {
-/*
+
 
     public static EmployeeAddress createEmployeeAddress(String staffId, Address address) {
 
         if (Helper.isNullOrEmpty(staffId))
+            return null;
+
+        if (address == null)
             return null;
 
         EmployeeAddress employeeAddress = new EmployeeAddress.Builder().
@@ -17,5 +28,5 @@ public class EmployeeAddressFactory {
                 build();
         return employeeAddress;
     }
- */
+
 }
--- src/main/java/za/ac/cput/schoolmanagement/repository/Employee/impl/EmployeeRepositoryimpl.java
+++ src/main/java/za/ac/cput/schoolmanagement/repository/Employee/impl/EmployeeRepositoryimpl.java
@@ -1,4 +1,48 @@
 package za.ac.cput.schoolmanagement.repository.Employee.impl;
 
-public class EmployeeRepositoryimpl {
+import za.ac.cput.schoolmanagement.domain.Employee;
+import za.ac.cput.schoolmanagement.repository.Employee.EmployeeRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class EmployeeRepositoryimpl implements EmployeeRepository {
+
+    private static EmployeeRepositoryimpl repository = null;
+    private List<Employee> employeeDB;
+
+    private EmployeeRepositoryimpl() {
+        employeeDB = new ArrayList<>();
+    }
+
+    public static EmployeeRepositoryimpl getRepository() {
+        if (repository == null) {
+            repository = new EmployeeRepositoryimpl();
+        }
+        return repository;
+    }
+
+    public Employee save (Employee employee) {
+        Optional<Employee> read = read(employee.getStaffId());
+        if (read.isPresent()) {
+            delete(read.get());
+        }
+        employeeDB.add(employee);
+        return employee;
+    }
+
+    public Optional<Employee> read (String staffId) {
+        return employeeDB.stream().
+                        filter(e -> e.getStaffId() == staffId).
+                        findAny();
+    }
+
+    public void delete (Employee employee) {
+        employeeDB.remove(employee);
+    }
+
+    public List<Employee> getAll (String staffId) {
+        return repository.getAll(staffId);
+    }
 }
