069f1f2d49e803e151dccc71871b614a96d6f2eb
Lihle_N <218193882@mycput.ac.za>
2022-06-13 15:47:20 +0200

    Implemented the save implementation in the service class

--- src/main/java/za/ac/cput/schoolmanagement/repository/country/CountryRepository.java
+++ src/main/java/za/ac/cput/schoolmanagement/repository/country/CountryRepository.java
@@ -8,10 +8,11 @@ package za.ac.cput.schoolmanagement.repository.country;
  */
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import za.ac.cput.schoolmanagement.domain.Country;
-
 import java.util.List;
 
+@Repository
 public interface CountryRepository extends JpaRepository<Country,String> {
     List<Country> findAll();
 
--- src/main/java/za/ac/cput/schoolmanagement/service/country/CountryService.java
+++ src/main/java/za/ac/cput/schoolmanagement/service/country/CountryService.java
@@ -1,4 +1,17 @@
 package za.ac.cput.schoolmanagement.service.country;
+/*
+ @Author: Lihle Njobe
+ Student number : 218193882
+ This is Country Service interface
+ Due date: 18 June 2022
+ */
 
-public interface CountryService {
+import za.ac.cput.schoolmanagement.domain.Country;
+import za.ac.cput.schoolmanagement.service.IService;
+
+import java.util.List;
+
+public interface CountryService extends IService<Country, String> {
+    List<Country> findAll();
+    void delete(String id);
 }
