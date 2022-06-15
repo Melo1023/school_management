37a040d29488a465c06df6ca0b61de04892e7366
Vuyisa <218192215@mycput.ac.za>
2022-06-15 22:17:12 +0200

    Third Commit

---
+++ src/main/java/za/ac/cput/schoolmanagement/service/name/NameService.java
@@ -0,0 +1,11 @@
+/* NameService.java
+   This is a Name Service Interface
+   Author: Vuyisa Nkangana
+   Student No: 218192215
+   Due Date: 18 June 2022
+ */
+package za.ac.cput.schoolmanagement.service.name;
+
+public interface NameService {
+
+}
