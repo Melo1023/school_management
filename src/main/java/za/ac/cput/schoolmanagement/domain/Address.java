397157b851756c578081251ca7ec52fe28b31e4f
DrMpahleni <216205999@mycput.ac.za>
2022-06-17 04:52:50 +0200

    service

--- src/main/java/za/ac/cput/schoolmanagement/domain/Address.java
+++ src/main/java/za/ac/cput/schoolmanagement/domain/Address.java
@@ -12,13 +12,13 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import java.io.Serializable;
+import java.util.Objects;
 
 @Entity
 public class Address implements Serializable  {
 
-    @Id
-    private String unitNumber;
 
+    private String unitNumber;
     private String complexName;
     private String streetNumber;
     private String streetName;
@@ -64,6 +64,19 @@ public class Address implements Serializable  {
     }
 
     @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Address address = (Address) o;
+        return postalCode == address.postalCode && unitNumber.equals(address.unitNumber) && complexName.equals(address.complexName) && streetNumber.equals(address.streetNumber) && streetName.equals(address.streetName) && city.equals(address.city);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(unitNumber, complexName, streetNumber, streetName, postalCode, city);
+    }
+
+    @Override
     public String toString() {
         return "Address{" +
                 "unitNumber='" + unitNumber + '\'' +
@@ -71,8 +84,9 @@ public class Address implements Serializable  {
                 ", streetNumber='" + streetNumber + '\'' +
                 ", streetName='" + streetName + '\'' +
                 ", postalCode=" + postalCode +
-               // ", city=" + city +
+                ", city=" + city +
                 '}';
+
     }
     public static class Builder {
 
@@ -123,9 +137,41 @@ public class Address implements Serializable  {
             this.streetNumber = streetNumber;
             this.streetName = streetName;
             this.postalCode = postalCode;
-            //this.city = city;
+            this.city = city;
 
             return this;
     }
+
+
+
+    }
+    public static class AddressId{
+        private String unitNumber,streetNumber;
+
+        public AddressId(String unitNumber, String streetNumber) {
+            this.unitNumber = unitNumber;
+            this.streetNumber = streetNumber;
+        }
+
+        public String getUnitNumber() {
+            return unitNumber;
+        }
+
+        public String getStreetNumber() {
+            return streetNumber;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            AddressId addressId = (AddressId) o;
+            return unitNumber.equals(addressId.unitNumber) && streetNumber.equals(addressId.streetNumber);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(unitNumber, streetNumber);
+        }
     }
 }
--- src/main/java/za/ac/cput/schoolmanagement/factory/AdressFactory.java
+++ src/main/java/za/ac/cput/schoolmanagement/factory/AdressFactory.java
@@ -9,23 +9,24 @@ package za.ac.cput.schoolmanagement.factory;
 
 import za.ac.cput.schoolmanagement.domain.Address;
 import za.ac.cput.schoolmanagement.domain.City;
-import za.ac.cput.schoolmanagement.helper.Helper;
+
 
 public class AdressFactory {
 
-    public static Address createAddress(String UnitNumber, String complexName, String streetNumber, String streetName, int postalCode, City city) {
-
-
-
-
-        Address address = new Address.Builder().
-                setUnitNumber(UnitNumber)
-                .setComplexName(complexName)
-                .setStreetNumber(streetNumber)
-                .setStreetName(streetName)
-                .setPostalCode(postalCode).
-                 setCity(city).
-                build();
+    public static Address  build(String UnitNumber, String complexName, String streetNumber, String streetName, Integer postalCode, City city) {
+        if(UnitNumber == null ||  UnitNumber.isEmpty())
+            throw new IllegalArgumentException("UnitNumber is required");
+        if( complexName == null ||  complexName.isEmpty())
+            throw new IllegalArgumentException("complexName is required");
+        if(streetNumber == null ||  streetNumber.isEmpty())
+            throw new IllegalArgumentException("streetNumber is required");
+        if( streetName == null ||  streetName.isEmpty())
+            throw new IllegalArgumentException("StreetName is required");
+        if(postalCode == null ||  postalCode==0)
+           throw new IllegalArgumentException("postalCode is required");
+        if( city == null )
+            throw new IllegalArgumentException("city is required");
+        Address address= new Address.Builder().setUnitNumber(UnitNumber).setComplexName(complexName).setStreetNumber(streetNumber).setStreetName(streetName).setPostalCode(postalCode).setCity(city).build();
         return address;
     }
 
