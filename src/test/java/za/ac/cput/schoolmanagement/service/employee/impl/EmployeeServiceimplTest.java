c3e73850c0b8c155b52840dc6058d7928c4c36c6
ianlouw <ianlouw1997@gmail.com>
2022-06-18 20:20:00 +0200

    resolving conflicts

--- src/test/java/za/ac/cput/schoolmanagement/service/employee/impl/EmployeeServiceimplTest.java
+++ src/test/java/za/ac/cput/schoolmanagement/service/employee/impl/EmployeeServiceimplTest.java
@@ -12,8 +12,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import za.ac.cput.schoolmanagement.domain.employee.Employee;
-import za.ac.cput.schoolmanagement.domain.Name;
-import za.ac.cput.schoolmanagement.domain.lookup.Name;
 import za.ac.cput.schoolmanagement.service.employee.EmployeeService;
 
 import java.util.List;
